fd7eea8862923db4ec436f0885fbdea76f179a69
xNikus <comlokoxd@gmail.com>
2023-11-01 22:57:16 +0100

    Update ScheduledTask.java to work again

--- src/main/java/pl/xnik3e/Guardian/components/ScheduledTask.java
+++ src/main/java/pl/xnik3e/Guardian/components/ScheduledTask.java
@@ -1,57 +1,44 @@
 package pl.xnik3e.Guardian.components;
 
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import pl.xnik3e.Guardian.GuardianDiscordBot;
+import pl.xnik3e.Guardian.Services.FireStoreService;
+import pl.xnik3e.Guardian.Utils.MessageUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class ScheduledTask {
 
-    private final Firestore firestore;
-    public final DocumentReference docRef;
-    private final List<Long> userIds = new ArrayList<>();
+    private final FireStoreService fireStoreService;
+    private final MessageUtils messageUtils;
     private final JDA jda;
+    private final List<String> userIds;
 
-    public ScheduledTask(Firestore firestore, GuardianDiscordBot bot) {
-        this.firestore = firestore;
-        this.docRef = firestore.collection("blacklist").document("toDelete");
+    @Autowired
+    public ScheduledTask(MessageUtils messageUtils, GuardianDiscordBot bot) {
+        this.messageUtils = messageUtils;
+        this.fireStoreService = messageUtils.getFireStoreService();
         this.jda = bot.getJda();
-        addSnapshotListener();
+        userIds = this.fireStoreService.getUserIds();
     }
 
-    private void addSnapshotListener() {
-        docRef.addSnapshotListener((snapshot, e) -> {
-            if (e != null) {
-                System.err.println("Listen failed: " + e);
-                return;
-            }
-            if (snapshot != null && snapshot.exists()) {
-                userIds.clear();
-                userIds.addAll((List<Long>) snapshot.get("userId"));
-            } else {
-                System.out.print("Current data: null");
-            }
-        });
-    }
 
     //schedule every 10 second
     //@Scheduled(fixedRate = 10 * 1000)
     //schedule every day at 22:46
-    @Scheduled(cron = "0 46 22 * * *")
+    //@Scheduled(cron = "0 46 22 * * *")
     public void notifyAllUsers(){
         Guild guild = jda.getGuildById(1112886826559078470L);
-        for(Long id : userIds){
+        for(String id : userIds){
             Member member = guild.getMemberById(id);
             if(member != null){
-                member.getUser().openPrivateChannel().queue(privateChannel -> {
-                    privateChannel.sendMessage("Witaj, zostałeś zbanowany na serwerze " + guild.getName() + " przez bota Guardian. Jeśli chcesz się odwołać, napisz do administracji serwera.").queue();
-                });
+                messageUtils.openPrivateChannelAndMessageUser(member,
+                        "Witaj, zostałeś zbanowany na serwerze " + guild.getName() + " przez bota Guardian. Jeśli chcesz się odwołać, napisz do administracji serwera.");
             }
         }
     }
