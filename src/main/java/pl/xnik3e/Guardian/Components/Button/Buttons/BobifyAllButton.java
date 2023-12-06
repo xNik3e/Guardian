4859ba25e66703e61155fcc413f8081845d217e7
xNikus <comlokoxd@gmail.com>
2023-12-06 14:55:33 +0100

    Refactor BobifyAllButton

--- src/main/java/pl/xnik3e/Guardian/Components/Button/Buttons/BobifyAllButton.java
+++ src/main/java/pl/xnik3e/Guardian/Components/Button/Buttons/BobifyAllButton.java
@@ -1,6 +1,7 @@
 package pl.xnik3e.Guardian.Components.Button.Buttons;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import pl.xnik3e.Guardian.Models.NickNameModel;
 import pl.xnik3e.Guardian.Models.ToBobifyMembersModel;
@@ -9,6 +10,7 @@ import pl.xnik3e.Guardian.Utils.MessageUtils;
 import pl.xnik3e.Guardian.Components.Button.IButton;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class BobifyAllButton implements IButton {
 
@@ -22,38 +24,53 @@ public class BobifyAllButton implements IButton {
 
     @Override
     public void handle(ButtonInteractionEvent event) {
-        if(!messageUtils.checkAuthority(event.getMember()))
+        if (!messageUtils.checkAuthority(event.getMember()))
             return;
         event.deferEdit().queue();
-        if (messageUtils.checkAuthority(event.getMember())) {
-            fireStoreService.fetchAllCache(ToBobifyMembersModel.class).ifPresentOrElse(model -> {
-                model.getMaps().forEach(map -> {
-                    String UID = map.get("userID");
-                    event.getGuild().retrieveMemberById(UID).queue(member -> {
-                        if (messageUtils.checkAuthority(member))
-                            return;
-                        NickNameModel nickNameModel = fireStoreService.fetchNickNameModel(member.getId());
-                        if (nickNameModel != null) {
-                            nickNameModel.getNickName().remove(member.getEffectiveName());
-                            fireStoreService.updateNickModel(nickNameModel);
-                        }
-                        messageUtils.bobify(member);
-                    });
+
+        fireStoreService.fetchAllCache(ToBobifyMembersModel.class)
+                .ifPresentOrElse(model -> {
+                    model.getMaps()
+                            .forEach(map -> {
+                                String UID = map.get("userID");
+                                Objects.requireNonNull(event.getGuild())
+                                        .retrieveMemberById(UID)
+                                        .queue(member -> {
+                                            new Thread(() -> bobifyMember(member)).start();
+                                        });
+                            });
+                    fireStoreService.deleteCacheUntilNow(ToBobifyMembersModel.class);
+                    sendSuccessMessage(event);
+                }, () -> {
+                    sendErrorMessage(event);
                 });
-                fireStoreService.deleteCacheUntilNow(ToBobifyMembersModel.class);
-                event.getHook().editOriginalComponents().queue();
-                event.getHook().editOriginalEmbeds(new EmbedBuilder()
-                        .setTitle("Success")
-                        .setDescription("Bobified all users")
-                        .setColor(Color.GREEN)
-                        .build()).queue();
-            }, () -> {
-                event.getHook().editOriginalComponents().queue();
-                event.getHook().editOriginalEmbeds(new EmbedBuilder().setTitle("Error")
-                        .setDescription("Data was not found")
-                        .setColor(Color.RED).build()).queue();
-            });
+    }
+
+    private static void sendSuccessMessage(ButtonInteractionEvent event) {
+        event.getHook().editOriginalComponents().queue();
+        event.getHook().editOriginalEmbeds(new EmbedBuilder()
+                .setTitle("Success")
+                .setDescription("Bobified all users")
+                .setColor(Color.GREEN)
+                .build()).queue();
+    }
+
+    private static void sendErrorMessage(ButtonInteractionEvent event) {
+        event.getHook().editOriginalComponents().queue();
+        event.getHook().editOriginalEmbeds(new EmbedBuilder().setTitle("Error")
+                .setDescription("Data was not found")
+                .setColor(Color.RED).build()).queue();
+    }
+
+    private void bobifyMember(Member member) {
+        if (messageUtils.checkAuthority(member))
+            return;
+        NickNameModel nickNameModel = fireStoreService.fetchNickNameModel(member.getId());
+        if (nickNameModel != null) {
+            nickNameModel.getNickName().remove(member.getEffectiveName());
+            fireStoreService.updateNickModel(nickNameModel);
         }
+        messageUtils.bobify(member);
     }
 
     @Override
